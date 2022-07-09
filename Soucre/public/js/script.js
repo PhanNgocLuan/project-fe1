faf7c66725542a540e792e624e62caf6deb37c7a
PhanNgocLuan <56753332+PhanNgocLuan@users.noreply.github.com>
2022-07-09 08:48:12 +0700

    Add files via upload

---
+++ Soucre/public/js/script.js
@@ -0,0 +1,21 @@
+var slideUp = {
+    distance: '150%',
+    origin: 'bottom',
+    opacity: null
+};
+
+ScrollReveal().reveal('.slide-up', slideUp);
+ScrollReveal({ reset: true });
+ScrollReveal().reveal('.lovers', {})
+ScrollReveal().reveal('.cat', {delay:300})
+ScrollReveal().reveal('.pet_care', {delay:600})
+ScrollReveal().reveal('.hedgehog',{delay:900})
+ScrollReveal().reveal('.pet_shop',{delay:1200})
+ScrollReveal().reveal('.headline_slide_1',{delay:100})
+ScrollReveal().reveal('.tagline_slide_1',{delay:500})
+ScrollReveal().reveal('.punchline_slide_1',{delay:800})
+ScrollReveal().reveal('.btn_animate_slide_1',{delay:1000})
+ScrollReveal().reveal('animate_col_1',{})
+ScrollReveal().reveal('animate_col_2',{delay:300})
+ScrollReveal().reveal('animate_col_3',{delay:600})
+ScrollReveal().reveal('animate_col_4',{delay:900})
